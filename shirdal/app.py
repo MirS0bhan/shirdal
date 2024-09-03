901a14c971d81a74f4ecf2871e98c181eb372561
misano <me@MirSobhan.ir>
2024-09-03 21:51:44 +0330

    add container to init and create app base

---
+++ shirdal/app.py
@@ -0,0 +1,19 @@
+from typing import Union, Callable
+
+from core import Container
+
+
+class Application:
+    def __init__(self):
+        self.container = Container()
+
+    def service(self, item: Union[object, Callable]) -> Union[object, Callable]:
+        """Decorator to register a service in the container."""
+        self.container.register(item)
+        return item  # Return the original item
+
+    def operate(self, dt):
+        """ Find endpoint and run it """
+        endpoint = dt.endpoint
+        f = self.container.resolve(endpoint)
+        f(dt)
