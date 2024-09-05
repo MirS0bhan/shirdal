3d10f9b6576f55758751932bc7c2b171533a42ec
misano <me@MirSobhan.ir>
2024-09-05 22:48:24 +0330

    separate broker

--- shirdal/app.py
+++ shirdal/app.py
@@ -4,17 +4,14 @@ from .core import Container, Broker
 
 
 class Application:
-    def __init__(self):
-        self.container = Container()
-        self.broker = Broker()
+    def __init__(self, brk: type[Broker]):
+        self.broker = brk
 
     def service(self, item: Union[object, Callable]) -> Union[object, Callable]:
         """Decorator to register a service in the container."""
-        self.container.register(item)
+        self.broker.co.register(item)
         return item  # Return the original item
 
     def operate(self, dt):
         """ Find endpoint and run it """
-        endpoint = dt.endpoint
-        f = self.container.resolve(endpoint)
-        self.broker.tm.add_task(lambda: f(dt))
+        self.broker.tm.add_task(dt)
